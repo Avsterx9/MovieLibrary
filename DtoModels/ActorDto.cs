deb8dd7572367cbe09dac48259dc408dc195e029
Avsterx9 <kchrobok.contact@gmail.com>
2022-06-22 15:44:42 +0200

    Add Dto Models for AutoMapper

---
+++ DtoModels/ActorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieLibrary.DtoModels
+{
+    public class ActorDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ DtoModels/CreateMovieDto.cs
@@ -0,0 +1,13 @@
+﻿using MovieLibrary.Entities;
+
+namespace MovieLibrary.DtoModels
+{
+    public class CreateMovieDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string ImgSrc { get; set; }
+    }
+}
